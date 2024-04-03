6b7b23e1873b345f89c3157ab512d523ebb0436f
afrizalwahyuadi66 <65825588+afrizalwahyuadi66@users.noreply.github.com>
2024-04-04 01:45:17 +0700

    Create program1.py
    Algoritma dan program yang dapat menerima input 3 buah bilangan Real sekaligus, kemudian menampilkan hasil perhitungan yang berupa penjumlahan dan perkalian ketiga bilangan tersebut.

---
+++ Tugas-UTS/program1.py
@@ -0,0 +1,23 @@
+# -------------------------------------------------------------------------
+  # JUDUL  : PROGRAM PERHITUNGAN 3 BILANGAN REAL 
+  # INPUT  : Angka ‘masukan 3 buah bilangan’
+  # PROSES : Memasukan 3 bilangan dan mengalikannya
+  # OUTPUT : Menampilkan perkalian dari 3 bilangan yang kita input
+# -------------------------------------------------------------------------
+
+# ALGORITMA----------------------------------------------------------------
+
+# Menerima input dari pengguna
+bilangan1 = float(input("Masukkan bilangan pertama: "))
+bilangan2 = float(input("Masukkan bilangan kedua: "))
+bilangan3 = float(input("Masukkan bilangan ketiga: "))
+
+# Melakukan perhitungan
+hasil_penjumlahan = bilangan1 + bilangan2 + bilangan3
+hasil_perkalian = bilangan1 * bilangan2 * bilangan3
+
+# Menampilkan hasil perhitungan
+print(f"Hasil Penjumlahan: {hasil_penjumlahan}")
+print(f"Hasil Perkalian  : {hasil_perkalian}")
+
+# -------------------------------------------------------------------------
